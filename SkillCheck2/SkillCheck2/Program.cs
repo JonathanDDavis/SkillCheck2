d7077bb6db92ddbd7dbfacdec934a1b39f533a4a
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-15 21:35:35 -0500

    Add files via upload

---
+++ SkillCheck2/SkillCheck2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SkillCheck2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions.print("how is this");
+        }
+  
+    }
+    class Functions
+    {
+        public static string print(string x)
+        {
+            Console.WriteLine(x);
+
+            return x;
+        }
+
+        public static string input(string x)
+        {
+            x = Console.ReadLine();
+
+            return x;
+
+        }
+        public static int input(int y)
+        {
+            y = int.Parse(Console.ReadLine());
+
+            return y;
+        }
+    }
+}
